c76c1c930d29d1643611c5ed7414e672f9aaca3d
Omoyeni <babatundeabike3@gmail.com>
2022-11-30 09:53:01 +0100

    Create MovieListViewController.swift

---
+++ MovieList/View/MovieListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MovieListViewController.swift
+//  MovieList
+//
+//  Created by Sharon Omoyeni Babatunde on 30/11/2022.
+//
+
+import UIKit
+
+class MovieListViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+}
