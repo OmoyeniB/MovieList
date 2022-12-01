760876e763d33d30005ad08dc358016d65a0c9a6
Omoyeni <babatundeabike3@gmail.com>
2022-12-01 20:28:05 +0100

    Create Constants.swift

---
+++ MovieList/Utilities/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  MovieList
+//
+//  Created by Sharon Omoyeni Babatunde on 30/11/2022.
+//
+
+import UIKit
+
+enum Constants {
+    
+    enum Alert {
+        static let errorTitle = "Error"
+    }
+    
+    enum Colors {
+        static let backgroundColor = UIColor(named: "BackgroundColor")
+        static let cellColor = UIColor(named: "CellColor")
+        static let textColor = UIColor(named: "TextColor")
+    }
+}
